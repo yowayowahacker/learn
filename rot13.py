59860dfe4ed31f3bc0dbb92781a8f97a4bfbbd62
yowayowahacker <61094906+yowayowahacker@users.noreply.github.com>
2020-02-16 06:07:38 +0900

    Update rot13.py

--- rot13.py
+++ rot13.py
@@ -7,16 +7,15 @@ while i < wordlen:
 
 	word_ord += 13
 	
-	if word_ord <= 60 or word_ord >= 71 and word_ord <= 77 or word_ord >= 104  and word_ord <= 109 or word_ord >= 136:
-		word_ord -= 13
+	if word_ord >= 65 and word_ord <= 90:
+		word_ord += 13
 
-	elif word_ord >= 61 and word_ord <= 70:
-		word_ord -= 10
-		if word_ord >= 58 and word_ord <= 60:
-			word_ord -= 10
-
-	elif word_ord >= 91 and word_ord <= 103 or word_ord >= 123:
-		word_ord -= 26
+		if word_ord >= 91:
+			word_ord -= 26
+	elif word_ord >= 97 and word_ord <= 122:
+		word_ord += 13
+		if word_ord >= 123:
+			word_ord -= 26
 
 	print(chr(word_ord),end="")
 	i += 1
