cf091b915defccf5bc4fb752d4cd75f2b87989cb
yowayowahacker <61094906+yowayowahacker@users.noreply.github.com>
2020-02-27 15:23:08 +0900

    Update autopass.py
    rot13を追加し、暗号化をして保存する

--- autopass.py
+++ autopass.py
@@ -26,4 +26,23 @@ else:
 print()
 use = input("用途/アカウント名を入力 ; ")
 print(*use ,end="/ ",file = codecs.open('autopass.txt' , 'a' , 'utf-8'))
-print(*pw ,end="\n", file = codecs.open('autopass.txt', 'a', 'utf-8'))
+
+wordlen = len(pw)
+i = 0
+while i < wordlen:
+	word_ord = ord(pw[i])
+
+	if word_ord >= 65 and word_ord <= 90:
+		word_ord += 13
+
+		if word_ord >= 91:
+			word_ord -= 26
+	elif word_ord >= 97 and word_ord <= 122:
+		word_ord += 13
+		if word_ord >= 123:
+			word_ord -= 26
+
+	print(*chr(word_ord) ,end="",file = codecs.open('autopass.txt' , 'a' , 'utf-8'))
+	i += 1
+	
+print(*"\n" ,end="",file = codecs.open('autopass.txt' , 'a' , 'utf-8'))
