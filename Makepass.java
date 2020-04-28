8d9a4f82a655b2fb18ebf9218edbd2680521b32c
Nescal <61094906+yowayowahacker@users.noreply.github.com>
2020-04-29 01:25:09 +0900

    Add files via upload

--- Makepass.java
+++ Makepass.java
@@ -9,7 +9,7 @@ public class Makepass
 	{
 		Scanner scan = new Scanner(System.in);
 		
-		//å®šæ•°ã‚’å®£è¨€ï¼ˆAsciiã‚³ãƒ¼ãƒ‰ï¼‰
+		//’è”‚ğéŒ¾iAsciiƒR[ƒhj
 		final int Asc_exc = 33;
 		final int Asc_0 = 48;
 		final int Asc_9 = 57;
@@ -19,22 +19,22 @@ public class Makepass
 		final int Asc_z = 122;
 		final int Asc_til = 126;
 
-		//å…¥åŠ›ã‚’å—ã‘ã‚‹
+		//“ü—Í‚ğó‚¯‚é
 		int len = 0;
 		String mark = "";
 		while(true)
 		{
 			try {
-				System.out.println("ãƒ‘ã‚¹ãƒ¯ãƒ¼ãƒ‰ã®æ¡æ•°ã‚’å…¥åŠ›");
+				System.out.println("ƒpƒXƒ[ƒh‚ÌŒ…”‚ğ“ü—Í");
 				len = Integer.parseInt(scan.next());
 				break;
 			}catch(NumberFormatException e) {
-				System.out.println("æ•°å­—ã§å…¥åŠ›ã—ã¦ãã ã•ã„");
+				System.out.println("”š‚Å“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
 				continue;
 			}
 		}
 		
-		System.out.println("è¨˜å·ï¼šæœ‰_y   ç„¡_n");
+		System.out.println("‹L†F—L_y   –³_n");
 		mark = scan.next();
 		scan.close();
 
@@ -62,15 +62,16 @@ public class Makepass
 		System.out.println(pass);
 		
 		String pastr = String.valueOf(pass);
-        	String pasha = "";
+        String pasha = "";
 		
 		try {
-            		MessageDigest digest = MessageDigest.getInstance("SHA-1");
-           		byte[] result = digest.digest(pastr.getBytes());
-           		 pasha = String.format("%040x", new BigInteger(1, result));
-        	} catch (Exception e){
-            		e.printStackTrace();
-        	}
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] result = digest.digest(pastr.getBytes());
+            pasha = String.format("%040x", new BigInteger(1, result));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+		
 		System.out.println(pasha);
 	}
 }
